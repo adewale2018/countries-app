a1800d16afb4b2f334651f5b4e7cc2d7c8cfdc0f
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-10-05 17:54:54 +0100

    Refactor and add spinner

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,34 +1,25 @@
 import React from 'react';
+import Country from './Country';
+import PropTypes from 'prop-types';
+import Spinner from './../Spinner/Spinner';
 import './Countries.css';
+
 
 const Countries = ({ datas, loading }) => {
   if(loading) {
-    return <h1 className="text-center text-danger">LOADING DATAS...</h1>
+    return <Spinner />
   }
   return (
-    <div className="Countries">
-      <div className="row">
-        {datas.map(data => (
-          <div className="col-md" key={data.name}>
-            <div id="card" className="card text-center text-dark border-info" style={    {width: '25rem', height: "45rem"}}>
-              <div className="card-header bg-transparent">
-                <h3>{data.name}</h3>
-              </div>
-              <img src={data.flag} className="card-img-top" alt={`${data.name} flag` } height={100}/>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">Capital: {data.capital}</li>
-                <li className="list-group-item">Calling Code: {data.callingCodes}</li>
-                <li className="list-group-item">Population: {data.population}</li>
-              </ul>
-              <div className="card-body">
-                <a href="#" className="btn btn-primary">Learn More</a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="Countries row">
+      {datas.map(data => (
+        <Country data={data} key={data.name}/>
+      ))}
     </div>
   );
+}
+Countries.propType = {
+  datas: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 export default Countries;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Navbar.css';
+import PropTypes from 'prop-types';
 
-
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div className="Navbar">
-        <nav className="navbar navbar-light bg-light">
-          <h1 className="navbar-brand">
-            <i className="fas fa-globe d-inline-block align-top"></i>
-          </h1>
-          <h1>WORLD INFO APP</h1>
-        </nav>
-      </div>
-    );
-  }
+const Navbar = ({ title, icon }) => {
+  return (
+    <header className="Nav">
+      <nav className="navbar bg-info">
+        <h1 className="navbar-brand">
+          <i className={`${icon} d-inline-block align-top`}></i>
+        </h1>
+        <h1>{title}</h1>
+      </nav>
+    </header>
+  );
 }
+Navbar.defaultProps = {
+  title: 'WORLD INFO APP',
+  icon: 'fas fa-globe'
+}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+export default Navbar;
